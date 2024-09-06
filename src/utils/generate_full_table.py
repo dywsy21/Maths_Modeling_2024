6773d62607a81d3c8b1b3d43035e8971fed4023a
dywsy21 <dywsy21@163.com>
2024-09-06 19:17:04 +0800

    added ipynb

--- src/utils/generate_full_table.py
+++ src/utils/generate_full_table.py
@@ -7,7 +7,7 @@ file_2 = pd.read_csv('附件/附件(csv)/附件2_2023年统计的相关数据.cs
 
 # add 地块类型 from 附件1_乡村的现有耕地 to 附件2_2023年的农作物种植情况
 def add_from_file_1():
-    the_csv_to_be_appended['地块类型'] = file_1['地块类型']
+    
 
 
 if __name__ == '__main__':
