2cebd9f0a2457449134d5e4cc0be49c5d82d722b
dywsy21 <dywsy21@163.com>
2024-09-06 18:00:09 +0800

    feat: add utility to generate full table in generate_full_table.py

---
+++ src/utils/generate_full_table.py
@@ -0,0 +1,9 @@
+import csv
+import pandas as pd
+
+the_csv_to_be_appended = pd.read_csv('附件/附件(csv)/附件2_2023年的农作物种植情况.csv', encoding='utf-8-sig')
+file_1 = pd.read_csv('附件/附件(csv)/附件1_乡村的现有耕地.csv', encoding='utf-8-sig')
+file_2 = pd.read_csv('附件/附件(csv)/附件2_2023年统计的相关数据.csv', encoding='utf-8-sig')
+
+# add 地块类型 from 附件1_乡村的现有耕地 to 附件2_2023年的农作物种植情况
+def add_from_file_1():
