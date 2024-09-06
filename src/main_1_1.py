f86c8f5a65030fd9e118957288eae7544c08ecbb
dywsy21 <dywsy21@163.com>
2024-09-06 21:23:30 +0800

    michellaneous changes

--- src/main_1_1.py
+++ src/main_1_1.py
@@ -2,16 +2,19 @@ import pandas as pd
 from pulp import LpMaximize, LpProblem, LpVariable, lpSum, LpBinary
 
 def main():
-    full_table = pd.read_csv('src\data\\full_table.csv')
+    full_table = pd.read_csv('src\\data\\full_table.csv')
 
-# create dict of land areas
-land_areas = dict(zip(full_table['种植地块'],full_table['种植面积/亩']))
+    # create dict of land areas
+    land_areas = dict(zip(full_table['种植地块'],full_table['种植面积/亩']))
 
     # create dict
     crop_prices = dict()
 
-
     linear_model = LpProblem(name="profit_maximization", sense=LpMaximize)
     
     # Create a sole decision variable: the number of hectares to plant with [each crop] in [each region] at [each year] at [each season]
-    x = LpVariable.dicts
+    x = LpVariable.dicts("planting_area", ((crop, region, year, season) for crop in full_table['作物'] for region in full_table['地区'] for year in full_table['年份'] for season in full_table['季节']), lowBound=0, cat='Continuous')
+
+
+if __name__ == '__main__':
+    main()
