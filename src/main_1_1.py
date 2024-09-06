f33232a4d70f5dcc2f7282cc329db746cd630a18
dywsy21 <dywsy21@163.com>
2024-09-06 21:47:27 +0800

    completed crop_to_condition dict

--- src/main_1_1.py
+++ src/main_1_1.py
@@ -5,8 +5,22 @@ def main():
     full_table = pd.read_csv('src\\data\\full_table.csv')
     file2 = pd.read_csv('附件\\附件(csv)\\附件1_乡村种植的农作物.csv')
 
+    crop_to_condition = {}
     for i, row in file2.iterrows():
+        text: str = row['种植耕地']
+        land_seasons = text.split(';')
+        land_seasons_dict: dict = {}
+        for land_season in land_seasons:
+            land_season = land_season.split(':')
+            land = land_season[0]
+            if len(land_season) > 1:
+                seasons = land_season[1].split(' ')
+                land_seasons_dict[land] = seasons
+            else:
+                land_seasons_dict[land] = ["第一季", "第二季"]
+        crop_to_condition[row['作物名称']] = land_seasons_dict
         
+
         
     
     # create dict of land areas
@@ -17,15 +31,6 @@ def main():
     # Create a sole decision variable: the number of hectares to plant with [each crop] in [each region] at [each year] at [each season]
     x = LpVariable.dicts("planting_area", ((crop, region, year, season) for crop in full_table['作物名称'] for region in full_table['种植地块'].unique() for year in range(2024, 2031) for season in full_table['种植季次'].unique()), lowBound=0, cat='Continuous')
 
-    text: str = file2['种植耕地']
-    land_seasons = text.split(';')
-    land_seasons_dict: dict = {}
-    for land_season in land_seasons:
-        land_season = land_season.split(':')
-        land = land_season[0]
-        if len(land_season) > 1:
-            seasons = land_season[1].split(' ')
-            land_seasons_dict[land] = seasons
 
 if __name__ == '__main__':
     main()
