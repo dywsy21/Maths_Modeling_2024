bc3cd851d2bcbf0bda5d1ff0c353ab1ad60ead42
antennaterry <terry041020@163.com>
2024-09-06 21:07:18 +0800

    land_areas dict

--- src/main_1_1.py
+++ src/main_1_1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pulp import LpMaximize, LpProblem, LpVariable, lpSum, LpBinary
 
-full_table = pd.read_csv('data/full_table.csv')
+full_table = pd.read_csv('src\data\\full_table.csv')
 
-# create dict
-crop_prices = dict()
+# create dict of land areas
+land_areas = dict(zip(full_table['种植地块'],full_table['种植面积/亩']))
