f08d77643d8a1e922a437dc9e20cdf0e919e4fff
dywsy21 <dywsy21@163.com>
2024-09-06 21:00:31 +0800

    changed reading

--- src/main_1_1.py
+++ src/main_1_1.py
@@ -1,11 +1,7 @@
 import pandas as pd
 from pulp import LpMaximize, LpProblem, LpVariable, lpSum, LpBinary
 
-# read data from 4 csv files
-file1 = pd.read_csv('附件\附件(csv)\附件1_乡村的现有耕地.csv')
-file2 = pd.read_csv('附件\附件(csv)\附件1_乡村种植的农作物.csv')
-file3 = pd.read_csv('附件\附件(csv)\附件2_2023年的农作物种植情况.csv')
-file4 = pd.read_csv('附件\附件(csv)\附件2_2023年统计的相关数据.csv')
+full_table = pd.read_csv('data/full_table.csv')
 
 # create dict
 crop_prices = dict()
