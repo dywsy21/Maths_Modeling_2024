0e889a5541646b7a308343d219b2d6609e465f59
dywsy21 <dywsy21@163.com>
2024-09-06 21:49:11 +0800

    added constraints msg

--- src/main_1_1.py
+++ src/main_1_1.py
@@ -31,6 +31,37 @@ def main():
     # Create a sole decision variable: the number of hectares to plant with [each crop] in [each region] at [each year] at [each season]
     x = LpVariable.dicts("planting_area", ((crop, region, year, season) for crop in full_table['作物名称'] for region in full_table['种植地块'].unique() for year in range(2024, 2031) for season in full_table['种植季次'].unique()), lowBound=0, cat='Continuous')
 
+    # 加十个约束条件：
+    # 1. 平旱地、梯田和山坡地每年适宜单季种植粮食类作物（水稻除外）。
+
+
+    # 2. 水浇地每年可以单季种植水稻或两季种植蔬菜作物。
+
+
+    # 3. 若在某块水浇地种植两季蔬菜，第一季可种植多种蔬菜（大白菜、白萝卜和红萝卜除外）；第二季只能种植大白菜、白萝卜和红萝卜中的一种（便于管理）。
+
+
+    # 4. 根据季节性要求，大白菜、白萝卜和红萝卜只能在水浇地的第二季种植。
+
+
+    # 5. 普通大棚每年种植两季作物，第一季可种植多种蔬菜（大白菜、白萝卜和红萝卜除外），第二季只能种植食用菌。
+
+
+    # 6. 因食用菌类适应在较低且适宜的温度和湿度环境中生长，所以只能在秋冬季的普通大棚里种植。
+
+
+    # 7. 智慧大棚每年都可种植两季蔬菜（大白菜、白萝卜和红萝卜除外）。
+
+
+    # 8. 从 2023 年开始要求每个地块（含大棚）的所有土地三年内至少种植一次豆类作物。
+
+
+    # 9. 每种作物每季的种植地不能太分散。
+
+
+    # 10. 每种作物在单个地块（含大棚）种植的面积不宜太小。
+
+
 
 if __name__ == '__main__':
     main()
