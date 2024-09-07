7f803cbedd1e60daee2a1083f4d4eb9d4139228e
dywsy21 <dywsy21@163.com>
2024-09-07 21:27:01 +0800

    revised get_price

--- src/main_1_1.py
+++ src/main_1_1.py
@@ -162,7 +162,7 @@ def main(reduction_factor, index):
     
     def get_price(crop, season):
         for i, row in full_table.iterrows():
-            if row['作物名称'] == crop and row['种植地块'] == region:
+            if row['作物名称'] == crop and row['种植季次'] == season:
                 return row['平均价格/(元/斤)']
         return 0
 
