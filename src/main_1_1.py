65d9bda84c9782227e5994f25dbee5d43b94a4c1
dywsy21 <dywsy21@163.com>
2024-09-07 10:15:13 +0800

    Revert "renamed decision var"
    This reverts commit f58fa5c3898051294c73d8d633e0331a781d80b6.

--- src/main_1_1.py
+++ src/main_1_1.py
@@ -12,7 +12,7 @@ def main(reduction_factor, index):
     linear_model = LpProblem(name="profit_maximization", sense=LpMaximize)
     
     # Create a sole decision variable: the number of hectares to plant with [each crop] in [each region] at [each year] at [each season]
-    planting_area = LpVariable.dicts("planting_area", ((crop, region, year, season) for crop in full_table['作物名称'] for region in full_table['种植地块'].unique() for year in years for season in full_table['种植季次'].unique()), lowBound=0, cat='Continuous')
+    x = LpVariable.dicts("planting_area", ((crop, region, year, season) for crop in full_table['作物名称'] for region in full_table['种植地块'].unique() for year in years for season in full_table['种植季次'].unique()), lowBound=0, cat='Continuous')
 
     # 加十三个约束条件：
     # 1. 平旱地、梯田和山坡地每年适宜单季种植粮食类作物（水稻除外）。 [已被12包含]
@@ -29,16 +29,16 @@ def main(reduction_factor, index):
     # 3. 若在某块水浇地种植两季蔬菜，第一季可种植多种蔬菜（大白菜、白萝卜和红萝卜除外）；第二季只能种植大白菜、白萝卜和红萝卜中的一种（便于管理）。
     for region in full_table['种植地块'].unique():
         for year in years:
-            linear_model += lpSum(planting_area[crop, region, year, '第一季'] for crop in full_table['作物名称'].unique() if crop not in ['大白菜', '白萝卜', '红萝卜']) <= 1
-            linear_model += lpSum(planting_area[crop, region, year, '第二季'] for crop in full_table['作物名称'].unique() if crop in ['大白菜', '白萝卜', '红萝卜']) <= 1
-            linear_model += lpSum(planting_area[crop, region, year, '第二季'] for crop in full_table['作物名称'].unique() if crop not in ['大白菜', '白萝卜', '红萝卜']) == 0
+            linear_model += lpSum(x[crop, region, year, '第一季'] for crop in full_table['作物名称'].unique() if crop not in ['大白菜', '白萝卜', '红萝卜']) <= 1
+            linear_model += lpSum(x[crop, region, year, '第二季'] for crop in full_table['作物名称'].unique() if crop in ['大白菜', '白萝卜', '红萝卜']) <= 1
+            linear_model += lpSum(x[crop, region, year, '第二季'] for crop in full_table['作物名称'].unique() if crop not in ['大白菜', '白萝卜', '红萝卜']) == 0
             
 
     # 4. 根据季节性要求，大白菜、白萝卜和红萝卜只能在水浇地的第二季种植。
     for year in years:
         for i, row in full_table.iterrows():
             if row['作物名称'] in ['大白菜', '白萝卜', '红萝卜'] and row['地块类型'] == '水浇地':
-                linear_model += planting_area[row['作物名称'], row['种植地块'], year, '第一季'] == 0
+                linear_model += x[row['作物名称'], row['种植地块'], year, '第一季'] == 0
                 
 
     # 5. 普通大棚每年种植两季作物，第一季可种植多种蔬菜（大白菜、白萝卜和红萝卜除外），第二季只能种植食用菌。[已被12包含]
@@ -55,14 +55,14 @@ def main(reduction_factor, index):
     bean_crops = ['黄豆', '黑豆', '红豆', '绿豆', '爬豆', '豇豆', '刀豆', '芸豆']
     for region in full_table['种植地块']:
         for y_begin in range(2024, 2029):
-            linear_model += lpSum(planting_area[crop, region, year, season] for crop in bean_crops 
+            linear_model += lpSum(x[crop, region, year, season] for crop in bean_crops 
                                   for year in range(y_begin, y_begin+3) for season in seasons) > 0
 
     # 9. 每种作物每季的种植地不能太分散。我们限制最大种植地块数为 5。
     small_value = 0
     for crop in full_table['作物名称'].unique():
         for season in full_table['种植季次'].unique():
-            linear_model += lpSum((planting_area[crop, region, year, season] > small_value) for region in full_table['种植地块'].unique() for year in years) <= 5
+            linear_model += lpSum((x[crop, region, year, season] > small_value) for region in full_table['种植地块'].unique() for year in years) <= 5
 
 
 
@@ -71,14 +71,14 @@ def main(reduction_factor, index):
         for region in full_table['种植地块']:
             for year in years:
                 for season in seasons:
-                    linear_model += (planting_area[crop, region, year, season] >= 0.3*region_areas[region])
+                    linear_model += (x[crop, region, year, season] >= 0.3*region_areas[region])
         
 
     # 11. 不能超出地块面积
     for region in full_table['种植地块'].unique():
         for year in years:
             for season in seasons:
-                linear_model += lpSum(planting_area[crop, region, year, season] for crop in full_table['作物名称'].unique()) <= region_areas[region]
+                linear_model += lpSum(x[crop, region, year, season] for crop in full_table['作物名称'].unique()) <= region_areas[region]
 
 
     # 12. 每种作物须满足相应的种植条件
@@ -103,17 +103,17 @@ def main(reduction_factor, index):
                 for season in full_table['种植季次'].unique():
                     if region in crop_to_condition[crop]:
                         if season not in crop_to_condition[crop][region]:
-                            linear_model += planting_area[(crop, region, year, season)] == 0
+                            linear_model += x[(crop, region, year, season)] == 0
                         elif crop_to_condition[crop][region] == ['单季']:
-                            linear_model += not (planting_area[(crop, region, year, '第一季')] and planting_area[(crop, region, year, '第二季')])
+                            linear_model += not (x[(crop, region, year, '第一季')] and x[(crop, region, year, '第二季')])
     
     # 13: 每种作物在同一地块（含大棚）都不能连续重茬种植，否则会减产
     for crop in full_table['作物名称'].unique():
         for region in full_table['种植地块'].unique():
             for year in years:
-                linear_model += ((planting_area[crop, region, year, '第一季']>0) & (planting_area[crop, region, year, '第二季']>0)) == 0
+                linear_model += ((x[crop, region, year, '第一季']>0) & (x[crop, region, year, '第二季']>0)) == 0
                 if year < 2030:
-                    linear_model += ((planting_area[crop, region, year, '第二季']>0) & (planting_area[crop, region, year+1, '第一季']>0)) == 0
+                    linear_model += ((x[crop, region, year, '第二季']>0) & (x[crop, region, year+1, '第一季']>0)) == 0
 
 
 
@@ -135,8 +135,8 @@ def main(reduction_factor, index):
 
     # 目标函数
     # 超出预期销售量的部分，价格乘以 reduction_factor
-    linear_model += lpSum(planting_area[crop, region, year, season] * (max(crop_to_price[crop] * get_yield(crop, region) * (1 - reduction_factor) - crop_to_cost[crop], 0)
-                            if planting_area[crop, region, year, season] > crop_to_expected_sales[crop] 
+    linear_model += lpSum(x[crop, region, year, season] * (max(crop_to_price[crop] * get_yield(crop, region) * (1 - reduction_factor) - crop_to_cost[crop], 0)
+                            if x[crop, region, year, season] > crop_to_expected_sales[crop] 
                             else crop_to_price[crop] * get_yield(crop, region) - crop_to_cost[crop]) 
                             for crop in full_table['作物名称'].unique()
                             for region in full_table['种植地块'].unique() 
